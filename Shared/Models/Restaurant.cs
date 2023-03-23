38bc4ff19e0151c3edd1b0e692a15664fc0db9b5
younes888796 <127496533+younes888796@users.noreply.github.com>
2023-03-23 08:42:32 +0000

    younesse 23-03-23

---
+++ Shared/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+namespace Formation.Models
+{
+    public class Restaurant
+    {
+       
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+     
+        public string Address { get; set; }
+        [Required]
+   
+        public string PhoneNumber { get; set; }
+    }
+}
